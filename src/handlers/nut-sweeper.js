f6d953f7763490713e52f7d61d919bd5d2f7f7e3
Jason Clark <jason@jjasonclark.com>
2019-11-28 15:37:43 -0800

    delete follow up nuts older than timeout and initial nuts older than timeout not being used

--- src/handlers/nut-sweeper.js
+++ src/handlers/nut-sweeper.js
@@ -6,8 +6,12 @@ const { db } = require('../lib/db/db');
 const handler = async (event, context) => {
   try {
     logger.info({ event, context }, 'Starting handler');
-    await db.none("DELETE FROM nuts WHERE created <= NOW() - INTERVAL '1 DAY'");
-    await db.none('DELETE FROM nuts WHERE issued IS NOT NULL');
+    await db.none(
+      "DELETE FROM nuts WHERE initial IS NOT NULL AND created <= NOW() - INTERVAL '1 DAY'"
+    );
+    await db.none(
+      "DELETE FROM nuts WHERE initial IS NULL AND created <= NOW() - INTERVAL '1 DAY' AND id NOT IN (SELECT initial AS id FROM nuts WHERE created >= NOW() - INTERVAL '1 DAY')"
+    );
     return { success: true };
   } catch (error) {
     logger.error(error);
